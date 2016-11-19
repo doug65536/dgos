59c76410376ce6360fa3593f86287df83ed2e232
Doug Gale <doug16k@gmail.com>
2016-11-18 19:50:25 -0500

    - Fix parallel make again - Start storage drivers

---
+++ kernel/lib/dev_storage.c
@@ -0,0 +1,1 @@
+#include "dev_storage.h"
---
+++ kernel/lib/dev_storage.h
@@ -0,0 +1,45 @@
+#pragma once
+
+// Storage device interface
+
+#include "dev_storage.h"
+
+//
+// Storage Device
+
+typedef struct storage_dev_t storage_dev_t;
+
+typedef struct storage_dev_vtbl_t storage_dev_vbtl_t;
+
+typedef struct storage_dev_base_t {
+    storage_dev_vbtl_t *vtbl;
+} storage_dev_base_t;
+
+struct storage_dev_vbtl_t {
+    int (*detect)(storage_dev_base_t **result);
+
+    void (*init)(storage_dev_base_t *);
+    void (*cleanup)(storage_dev_base_t *);
+
+
+};
+
+//
+// Storage Interface
+
+typedef struct storage_if_t storage_if_t;
+
+typedef struct storage_if_vtbl_t storage_if_vbtl_t;
+
+typedef struct storage_if_base_t {
+    storage_if_vbtl_t *vtbl;
+} storage_if_base_t;
+
+struct storage_if_vbtl_t {
+    int (*detect)(storage_if_base_t **result);
+
+    void (*init)(storage_if_base_t *);
+    void (*cleanup)(storage_if_base_t *);
+
+    int (*detect_devices)(storage_dev_base_t **result);
+};
