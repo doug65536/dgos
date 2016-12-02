1f2cb5b335037945d3dc6dec46ebb16516a1af13
Doug Gale <doug16k@gmail.com>
2016-12-02 03:24:23 -0500

    Remove old junk in the wrong place

--- kernel/arch/cpu.h
+++ kernel/arch/cpu.h
@@ -2,9 +2,3 @@
 
 void cpu_init(int ap);
 void cpu_hw_init(void);
-
-int irq_hook(int irq, void (*handler)(void));
-int irq_unhook(int irq, void (*handler)(void));
-
-int irq_is_pending(int irq);
-int irq_ack(int irq);
