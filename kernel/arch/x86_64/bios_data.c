4f5535aba35deec05ec4c6d57399113cd7f05e6e
Doug Gale <doug16k@gmail.com>
2016-12-02 03:23:48 -0500

    Callout handler for zero page remap

--- kernel/arch/x86_64/bios_data.c
+++ kernel/arch/x86_64/bios_data.c
@@ -1,3 +1,15 @@
 #include "bios_data.h"
+#include "mm.h"
+#include "callout.h"
 
 char *zero_page;
+
+static void biosdata_remap(void *arg)
+{
+    (void)arg;
+    zero_page = mmap(
+            0, PAGESIZE, PROT_READ | PROT_WRITE,
+            MAP_PHYSICAL, -1, 0);
+}
+
+REGISTER_CALLOUT(biosdata_remap, 0, 'V', "000");
