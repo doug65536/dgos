c502d57b1398aab1b8861bf978a57775c71ff4b9
Doug Gale <doug16k@gmail.com>
2016-12-07 20:41:34 -0500

    memmove bug fixed

--- boot/string.c
+++ boot/string.c
@@ -113,7 +113,7 @@ void *memmove(void *dest, void const *src, size_t n)
         return memcpy(d, s, n);
 
     if (d > s) {
-        for (size_t i = n; i; --n)
+        for (size_t i = n; i; --i)
             d[i-1] = s[i-1];
     }
 
