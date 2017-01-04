804c884991c9814d564f566111fa8373223035da
Doug Gale <doug16k@gmail.com>
2017-01-04 06:15:32 -0500

    - Implement realloc - Define constant for malloc overhead - Define page size shift in stdlib.h

--- kernel/lib/stdlib.c
+++ kernel/lib/stdlib.c
@@ -1,11 +1,24 @@
 #include "stdlib.h"
 #include "string.h"
 #include "mm.h"
+#include "assert.h"
 
 typedef struct blk_hdr_t {
     size_t sig;
     size_t size;
 } blk_hdr_t;
+
+C_ASSERT(sizeof(blk_hdr_t) == _MALLOC_OVERHEAD);
+
+static void chk_sig(blk_hdr_t *p)
+{
+    assert(p->sig == (~p->size ^ (uintptr_t)p));
+}
+
+static void set_sig(blk_hdr_t *p)
+{
+    p->sig = ~p->size ^ (uintptr_t)p;
+}
 
 void *malloc(size_t size)
 {
@@ -14,7 +27,7 @@ void *malloc(size_t size)
                         0, -1, 0);
 
     p->size = size;
-    p->sig = ~size ^ (size_t)p;
+    set_sig(p);
 
     return p + 1;
 }
@@ -28,11 +41,51 @@ void *calloc(size_t num, size_t size)
     return p;
 }
 
+void *realloc(void *p, size_t new_size)
+{
+    // Call malloc if the old pointer is null
+    if (!p)
+        return malloc(new_size);
+
+    blk_hdr_t *h = (blk_hdr_t*)p - 1;
+
+    // Do nothing if size didn't actually change
+    if (new_size == h->size)
+        return p;
+
+    chk_sig(h);
+
+    // Get old and new page count
+    size_t old_pagecount = (h->size + (PAGE_SIZE - 1)) >> PAGE_SCALE;
+    size_t new_pagecount = (new_size + (PAGE_SIZE - 1)) >> PAGE_SCALE;
+
+    if (old_pagecount != new_pagecount) {
+        // Size changed significantly
+
+        // Remap pages
+        p = mremap(h,
+                   old_pagecount << PAGE_SCALE,
+                   new_pagecount << PAGE_SCALE,
+                   MREMAP_MAYMOVE);
+
+        if (!p)
+            return 0;
+
+        h = (blk_hdr_t*)p - 1;
+        h->size = new_size;
+    } else {
+        h->size = new_size;
+    }
+    set_sig(h);
+
+    return p;
+}
+
 void free(void *p)
 {
     blk_hdr_t *h = (blk_hdr_t*)p - 1;
 
-    if (h->sig != (~h->size ^ (size_t)p)) {
+    if (h->sig != (~h->size ^ (uintptr_t)p)) {
         munmap(h, h->size + sizeof(*h));
         return;
     }
--- kernel/lib/stdlib.h
+++ kernel/lib/stdlib.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "types.h"
 
+#ifndef PAGE_SCALE
+#define PAGE_SCALE  12
+#endif
+
 #ifndef PAGE_SIZE
 #define PAGE_SIZE   4096U
 #endif
+
+#define _MALLOC_OVERHEAD    16
 
 __attribute__((malloc))
 void *calloc(size_t num, size_t size);
 
 __attribute__((malloc))
 void *malloc(size_t size);
+
+void *realloc(void *p, size_t new_size);
 
 void free(void *p);
 
