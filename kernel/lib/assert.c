e5b6dbae37fc976b41ec41fd625ad47bd4b11134
Doug Gale <doug16k@gmail.com>
2017-02-10 11:08:07 -0500

    Add assert_msg macro which takes a message string

--- kernel/lib/assert.c
+++ kernel/lib/assert.c
@@ -2,9 +2,11 @@
 #include "printk.h"
 
 void assert_failed(char const *expr,
+                   char const *msg,
                    char const *file,
                    int line)
 {
-    printdbg("\n** ASSERT FAILED: %s(%d): %s\n",
-           file, line, expr);
+    printdbg("\n** ASSERT FAILED: %s(%d): %s %s\n",
+           file, line, expr, msg ? msg : "");
 }
+
--- kernel/lib/assert.h
+++ kernel/lib/assert.h
@@ -2,10 +2,14 @@
 #include "likely.h"
 
 void assert_failed(char const *expr,
-                  char const *file,
-                  int line);
+                   char const *msg,
+                   char const *file,
+                   int line);
 
 #define assert(e) \
-    (likely(e) ? (void)0 : assert_failed(#e, __FILE__, __LINE__))
+    (likely(e) ? (void)0 : assert_failed(#e, 0, __FILE__, __LINE__))
+
+#define assert_msg(e, msg) \
+    (likely(e) ? (void)0 : assert_failed(#e, (msg), __FILE__, __LINE__))
 
 #define C_ASSERT(e) typedef char __C_ASSERT__[(e)?1:-1]
