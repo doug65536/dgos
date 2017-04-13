f111c9702b2fb6e18eae1705c933acb23bfa5655
Doug Gale <doug16k@gmail.com>
2017-04-12 22:31:31 -0400

    - Make bitsearch.h API completely constexpr

--- kernel/lib/bitsearch.h
+++ kernel/lib/bitsearch.h
@@ -3,43 +3,43 @@
 #include "type_traits.h"
 
 // Return bit number of least significant set bit
-static inline uint8_t bit_lsb_set_32(int32_t n)
+static constexpr inline uint8_t bit_lsb_set_32(int32_t n)
 {
     return __builtin_ctz(n);
 }
 
 // Return bit number of least significant set bit
-static inline uint8_t bit_lsb_set_64(int64_t n)
+static constexpr inline uint8_t bit_lsb_set_64(int64_t n)
 {
     return __builtin_ctzl(n);
 }
 
 // Return bit number of most significant set bit
-static inline uint8_t bit_msb_set_32(int32_t n)
+static constexpr inline uint8_t bit_msb_set_32(int32_t n)
 {
     return 31 - __builtin_clz(n);
 }
 
 // Return bit number of most significant set bit
-static inline uint8_t bit_msb_set_64(int64_t n)
+static constexpr inline uint8_t bit_msb_set_64(int64_t n)
 {
     return 63 - __builtin_clzl(n);
 }
 
 template<typename T>
-static inline uint8_t bit_lsb_set(T n, false_type)
+static constexpr inline uint8_t bit_lsb_set(T n, false_type)
 {
     return bit_lsb_set_32(int32_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_lsb_set(T n, true_type)
+static constexpr inline uint8_t bit_lsb_set(T n, true_type)
 {
     return bit_lsb_set_64(int64_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_lsb_set(T n)
+static constexpr inline uint8_t bit_lsb_set(T n)
 {
     return bit_lsb_set(n, typename conditional<
                        sizeof(T) == sizeof(uint64_t),
@@ -47,19 +47,19 @@ static inline uint8_t bit_lsb_set(T n)
 }
 
 template<typename T>
-static inline uint8_t bit_msb_set(T n, false_type)
+static constexpr inline uint8_t bit_msb_set(T n, false_type)
 {
     return bit_msb_set_32(int32_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_msb_set(T n, true_type)
+static constexpr inline uint8_t bit_msb_set(T n, true_type)
 {
     return bit_msb_set_64(int64_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_msb_set(T n)
+static constexpr inline uint8_t bit_msb_set(T n)
 {
     return bit_msb_set(n, typename conditional<
                        sizeof(T) == sizeof(uint64_t),
@@ -67,33 +67,33 @@ static inline uint8_t bit_msb_set(T n)
 }
 
 // return ceil(log(n) / log(2))
-static inline uint8_t bit_log2_n_32(int32_t n)
+static constexpr inline uint8_t bit_log2_n_32(int32_t n)
 {
     uint8_t top = bit_msb_set_32(n);
     return top + !!(~(uint32_t(-1) << top) & n);
 }
 
 // return ceil(log(n) / log(2))
-static inline uint8_t bit_log2_n_64(int64_t n)
+static constexpr inline uint8_t bit_log2_n_64(int64_t n)
 {
     uint8_t top = bit_msb_set_64(n);
     return top + !!(~(uint64_t(-1) << top) & n);
 }
 
 template<typename T>
-static inline uint8_t bit_log2_n(T n, false_type)
+static constexpr inline uint8_t bit_log2_n(T n, false_type)
 {
     return bit_log2_n_32(int32_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_log2_n(T n, true_type)
+static constexpr inline uint8_t bit_log2_n(T n, true_type)
 {
     return bit_log2_n_64(int64_t(n));
 }
 
 template<typename T>
-static inline uint8_t bit_log2_n(T n)
+static constexpr inline uint8_t bit_log2_n(T n)
 {
     return bit_log2_n(n, typename conditional<
                       sizeof(T) == sizeof(uint64_t),
