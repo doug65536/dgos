439e4906208712653a43c5067500ebcb7e20af3a
Doug Gale <doug16k@gmail.com>
2017-04-25 03:42:51 -0400

    - Add helpers to allow bitwise operations on enum struct types

---
+++ kernel/lib/bitop.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "type_traits.h"
+
+template<typename T>
+constexpr bool enable_bitwise(T)
+{
+    return false;
+}
+
+template<typename E>
+constexpr E& operator|=(E& lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    using underlying = typename underlying_type<E>::type;
+
+    return lhs = static_cast<E>(static_cast<underlying>(lhs) |
+            static_cast<underlying>(rhs));
+}
+
+template<typename E>
+constexpr E& operator&=(E& lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    using underlying = typename underlying_type<E>::type;
+
+    return lhs = static_cast<E>(static_cast<underlying>(lhs) &
+                          static_cast<underlying>(rhs));
+}
+
+template<typename E>
+constexpr E& operator^=(E& lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    using underlying = typename underlying_type<E>::type;
+
+    return lhs = static_cast<E>(static_cast<underlying>(lhs) ^
+                          static_cast<underlying>(rhs));
+}
+
+template<typename E>
+constexpr E operator~(E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    using underlying = typename underlying_type<E>::type;
+
+    return static_cast<E>(~static_cast<underlying>(rhs));
+}
+
+template<typename E>
+constexpr E operator|(E lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    return lhs |= rhs;
+}
+
+template<typename E>
+constexpr E operator&(E lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    return lhs &= rhs;
+}
+
+template<typename E>
+constexpr E operator^(E lhs, E rhs)
+{
+    static_assert(enable_bitwise(E{}),
+                  "Bitwise operators not enabled for this type");
+
+    return lhs ^= rhs;
+}
