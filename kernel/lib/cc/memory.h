c1d9125582c4095129faade0d2294835ed569a7f
Doug Gale <doug16k@gmail.com>
2017-04-28 05:21:39 -0400

    - Fix allocator rebind implementation and make allocate and deallocate const

--- kernel/lib/cc/memory.h
+++ kernel/lib/cc/memory.h
@@ -8,17 +8,23 @@ struct allocator
 {
     typedef _T value_type;
 
-    value_type * allocate(size_t __n)
+    template<typename _U>
+    struct rebind
+    {
+        using other = allocator<_U>;
+    };
+
+    value_type * allocate(size_t __n) const
     {
         return (value_type*)malloc(__n * sizeof(value_type));
     }
 
-    void deallocate(value_type * __p, size_t)
+    void deallocate(value_type * __p, size_t) const
     {
         free(__p);
     }
 
-    void destruct(value_type * __p)
+    void destruct(value_type * __p) const
     {
         __p->~value_type();
     }
@@ -29,13 +35,19 @@ struct page_allocator
 {
     typedef _T value_type;
 
-    value_type * allocate(size_t __n)
+    template<typename _U>
+    struct rebind
+    {
+        using other = allocator<_U>;
+    };
+
+    value_type * allocate(size_t __n) const
     {
         return mmap(nullptr, __n * sizeof(value_type),
                     PROT_READ | PROT_WRITE, 0, -1, 0);
     }
 
-    void deallocate(value_type * __p, size_t __n)
+    void deallocate(value_type * __p, size_t __n) const
     {
         munmap(__p, __n * sizeof(value_type));
     }
