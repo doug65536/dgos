82eeae2ef3359ff61252dc1b9b7ada503380330c
Doug Gale <doug16k@gmail.com>
2017-05-05 08:19:57 -0400

    - Fix some type_traits and add helper safe_underlying_type which works for non-enumerations

--- kernel/lib/cc/type_traits.h
+++ kernel/lib/cc/type_traits.h
@@ -9,28 +9,29 @@ struct conditional
 template<typename _T, typename _F>
 struct conditional<true, _T, _F>
 {
-    typedef _T type;
+    using type = _T;
 };
 
 template<typename _T, typename _F>
 struct conditional<false, _T, _F>
 {
-    typedef _F type;
+    using type = _F;
 };
 
 template<typename _T, _T _Val>
 struct integral_constant
 {
-    typedef _T value_type;
-    typedef integral_constant<_T, _Val> type;
+    using value_type = _T;
+    using type = integral_constant<_T, _Val>;
+
     static constexpr _T value = _Val;
 
     operator value_type() const { return _Val; }
     value_type operator()() const { return _Val; }
 };
 
-typedef integral_constant<bool, true> true_type;
-typedef integral_constant<bool, false> false_type;
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
 
 template<bool _Val>
 struct bool_constant;
@@ -113,12 +114,15 @@ template<typename _T>
 using is_union = bool_constant<__is_union(_T)>;
 
 template<typename _T>
-struct is_lvalue_reference : public false_type
+struct is_lvalue_reference;
+
+template<typename _T>
+struct is_lvalue_reference<_T&> : public true_type
 {
 };
 
 template<typename _T>
-struct is_lvalue_reference<_T&&> : public true_type
+struct is_lvalue_reference<_T&&> : public false_type
 {
 };
 
@@ -136,33 +140,57 @@ struct enable_if
 template<typename _T>
 struct enable_if<true, _T>
 {
-    typedef _T type;
+    using type = _T;
 };
 
 template<typename _T>
 struct remove_reference
 {
-	typedef _T type;
+    using type = _T;
 };
 
 template<typename _T >
 struct remove_reference<_T&>
 {
-	typedef _T type;
+    using type = _T;
 };
 
 template<typename _T>
 struct remove_reference<_T&&>
 {
-	typedef _T type;
+    using type = _T;
 };
 
 template<int size, bool uns = true> struct type_from_size { };
-template<> struct type_from_size<1, true> { typedef uint8_t type; };
-template<> struct type_from_size<2, true> { typedef uint16_t type; };
-template<> struct type_from_size<4, true> { typedef uint32_t type; };
-template<> struct type_from_size<8, true> { typedef uint64_t type; };
-template<> struct type_from_size<1, false> { typedef int8_t type; };
-template<> struct type_from_size<2, false> { typedef int16_t type; };
-template<> struct type_from_size<4, false> { typedef int32_t type; };
-template<> struct type_from_size<8, false> { typedef int64_t type; };
+template<> struct type_from_size<1, true> { using type = uint8_t; };
+template<> struct type_from_size<2, true> { using type = uint16_t; };
+template<> struct type_from_size<4, true> { using type = uint32_t; };
+template<> struct type_from_size<8, true> { using type = uint64_t; };
+template<> struct type_from_size<1, false> { using type = int8_t; };
+template<> struct type_from_size<2, false> { using type = int16_t; };
+template<> struct type_from_size<4, false> { using type = int32_t; };
+template<> struct type_from_size<8, false> { using type = int64_t; };
+
+// Helper that gives underlying type for enums and is not an error otherwise
+template<typename T>
+class safe_underlying_type
+{
+private:
+    template<typename, typename>
+    struct helper;
+
+    template<typename U>
+    struct helper<U, true_type>
+    {
+        using type = typename underlying_type<U>::type;
+    };
+
+    template<typename U>
+    struct helper<U, false_type>
+    {
+        using type = U;
+    };
+
+public:
+    using type = typename helper<T, typename is_enum<T>::type>::type;
+};
