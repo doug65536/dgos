b15a9f329aa496a243897a2c316fe758a4b73e91
Doug Gale <doug16k@gmail.com>
2017-09-19 11:17:35 -0400

    A bit of work on shell

---
+++ user/shell/grammar.h
@@ -0,0 +1,8 @@
+#define TOKEN_EQ                               1
+#define TOKEN_PLUS                             2
+#define TOKEN_MINUS                            3
+#define TOKEN_TIMES                            4
+#define TOKEN_DIVIDE                           5
+#define TOKEN_QUOTED_STRING                    6
+#define TOKEN_IDENTIFIER                       7
+#define TOKEN_NUMBER                           8
