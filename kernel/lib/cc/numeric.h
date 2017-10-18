a252e25ab7d104a85f6ec1a763d19a4d1b8c7898
Doug Gale <doug16k@gmail.com>
2017-10-17 23:21:31 -0400

    - Add accumulate template

---
+++ kernel/lib/cc/numeric.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template<typename _InputIt, typename _T>
+_T accumulate(_InputIt __first, _InputIt __last, _T __init)
+{
+    for ( ; __first != __last; ++__first)
+        __init = __init + *__first;
+    return __init;
+}
+
+template<typename _InputIt, typename _T, typename _BinaryOperation>
+_T accumulate(_InputIt __first, _InputIt __last, _T __init,
+              _BinaryOperation __op)
+{
+    for ( ; __first != __last; ++__first)
+        __init = __op(__init, *__first);
+    return __init;
+}
