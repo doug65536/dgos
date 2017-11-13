a9c66ab588f1eb62565c5613f4fe9f9a348aad8f
Doug Gale <doug16k@gmail.com>
2017-11-13 10:42:16 -0500

    - Implement strncpy

--- libc/src/string/strncpy.cc
+++ libc/src/string/strncpy.cc
@@ -1,3 +1,18 @@
 #include <string.h>
 
-char *strncpy(char *restrict, const char *restrict, size_t);
+char *strncpy(char *restrict lhs, const char *restrict rhs, size_t sz)
+{
+    char *d = lhs;
+
+    size_t i = 0;
+
+    // Copy from src up to but not including null terminator
+    for ( ; i < sz && rhs[i]; ++i)
+        d[i] = rhs[i];
+
+    // Fill dest with zeros until at least sz bytes are written
+    for ( ; i < sz; ++i)
+        d[i] = 0;
+
+    return lhs;
+}
