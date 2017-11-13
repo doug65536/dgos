10934bf4168618ad08666db15af3efbafe2a4166
Doug Gale <doug16k@gmail.com>
2017-11-13 10:42:31 -0500

    - Implement strncmp

--- libc/src/string/strncmp.cc
+++ libc/src/string/strncmp.cc
@@ -1,3 +1,13 @@
 #include <string.h>
 
-int strncmp(const char *, const char *, size_t);
+int strncmp(char const *lhs, char const *rhs, size_t sz)
+{
+    int cmp = 0;
+    if (sz) {
+        do {
+            cmp = (unsigned char)(*lhs) -
+                    (unsigned char)(*rhs++);
+        } while (--sz && cmp == 0 && *lhs++);
+    }
+    return cmp;
+}
