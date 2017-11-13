a0133c217d856fd23d95f0040af541c62c40d75a
Doug Gale <doug16k@gmail.com>
2017-11-13 10:40:21 -0500

    - Declare variables accessed from assembly as extern "C"

--- boot/bootsect.h
+++ boot/bootsect.h
@@ -2,10 +2,10 @@
 
 #include "types.h"
 
-extern uint8_t boot_drive;
-extern uint32_t vbe_info_vector;
+extern "C" uint8_t boot_drive;
+extern "C" uint32_t vbe_info_vector;
 
 extern "C" void halt(char const*);
 
 uint16_t read_lba_sectors(char *buf, uint8_t drive,
-                         uint32_t lba, uint16_t count);
+                          uint32_t lba, uint16_t count);
