f9ada1795fe73a4f39fa90edab4188c898129480
Doug Gale <doug16k@gmail.com>
2017-11-13 10:53:47 -0500

    - Fix excessive line length

--- boot/part.h
+++ boot/part.h
@@ -3,20 +3,20 @@
 #include "types.h"
 
 struct partition_tbl_ent_t {
-	uint8_t  boot;					//0: Boot indicator bit flag: 0 = no, 0x80 = bootable (or "active")
-	uint8_t  start_head;			// H
+    uint8_t  boot;					// 0 = no, 0x80 = bootable (or "active")
+    uint8_t  start_head;			// H
 
-	uint16_t start_sector : 6;		// S
-	uint16_t start_cyl : 10;		// C
+    uint16_t start_sector : 6;		// S
+    uint16_t start_cyl : 10;		// C
 
-	uint8_t  system_id;
-	uint8_t  end_head;				// H
+    uint8_t  system_id;
+    uint8_t  end_head;				// H
 
-	uint16_t end_sector : 6;		// S
-	uint16_t end_cyl : 10;			// C
+    uint16_t end_sector : 6;		// S
+    uint16_t end_cyl : 10;			// C
 
-	uint32_t start_lba;
-	uint32_t total_sectors;
+    uint32_t start_lba;
+    uint32_t total_sectors;
 } __packed;
 
 extern partition_tbl_ent_t partition_table[];
