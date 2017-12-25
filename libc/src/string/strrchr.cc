86b473d2eed786996e2032304b844327f77d2457
Doug Gale <doug16k@gmail.com>
2017-12-25 04:09:16 -0500

    Implement libc strrchr

--- libc/src/string/strrchr.cc
+++ libc/src/string/strrchr.cc
@@ -1,3 +1,16 @@
 #include <string.h>
 
-//char *strrchr(const char *, int);
+char *strrchr(const char *s, int c)
+{
+    unsigned char const *us = (unsigned char const *)s;
+    unsigned char uc = (unsigned char)c;
+    unsigned char const *r = nullptr;
+    for (size_t i = 0; ; ++i) {
+        if (us[i] == uc)
+            r = (us + i);
+        if (us[i] == 0)
+            break;
+    }
+
+    return (char*)r;
+}
