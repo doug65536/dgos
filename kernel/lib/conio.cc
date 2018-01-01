abe2038dad7a3845d8712cf5269ef49c5392b2b0
Doug Gale <doug16k@gmail.com>
2017-12-31 23:59:37 -0500

    Formatting and dead code cleanup

--- kernel/lib/conio.cc
+++ kernel/lib/conio.cc
@@ -12,8 +12,7 @@ void con_clear(void)
 
 int con_cursor_toggle(int show)
 {
-    return console_display_vtbl.cursor_toggle(
-                console_display, show);
+    return console_display_vtbl.cursor_toggle(console_display, show);
 }
 
 void con_putc(int character)
@@ -24,20 +23,17 @@ void con_putc(int character)
 
 int con_print(char const *s)
 {
-    return console_display_vtbl.print(
-                console_display, s);
+    return console_display_vtbl.print(console_display, s);
 }
 
 int con_write(char const *s, intptr_t len)
 {
-    return console_display_vtbl.write(
-                console_display, s, len);
+    return console_display_vtbl.write(console_display, s, len);
 }
 
 void con_move_cursor(int dx, int dy)
 {
-    console_display_vtbl.mouse_add_xy(
-                console_display, dx, dy);
+    console_display_vtbl.mouse_add_xy(console_display, dx, dy);
 }
 
 int con_draw_xy(int x, int y, char const *s, int attr)
@@ -47,8 +43,7 @@ int con_draw_xy(int x, int y, char const *s, int attr)
         return console_display_vtbl.draw_xy(
                     console_display, x, y, s, attr);
     } else {
-        printdbg("Console draw too early @ %d,%d: %s\n",
-                 x, y, s);
+        printdbg("Console draw too early @ %d,%d: %s\n", x, y, s);
     }
     return 0;
 }
--- kernel/lib/mouse.cc
+++ kernel/lib/mouse.cc
@@ -4,6 +4,7 @@
 
 static int accum_x;
 static int accum_y;
+
 void mouse_event(mouse_raw_event_t event)
 {
     accum_x += event.hdist * 10;
@@ -20,18 +21,4 @@ void mouse_event(mouse_raw_event_t event)
 
     accum_x %= 45;
     accum_y %= 80;
-
-    //if (accum_x >= 0)
-    //    accum_x &= 7;
-    //else
-    //    accum_x = -((-accum_x) & 7);
-    //
-    //if (accum_y >= 0)
-    //    accum_y &= 7;
-    //else
-    //    accum_y = -((-accum_y) & 7);
-
-    //printk("Mouse h=%+4d v=%+4d w=%+2d b=%04x\n",
-    //       event.hdist, event.vdist,
-    //       event.wdist, event.buttons);
 }
