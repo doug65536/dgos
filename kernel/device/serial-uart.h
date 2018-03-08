c3201825cfd3846c1436bf5dbaaa53f13e9552b3
Doug Gale <doug16k@gmail.com>
2018-03-08 06:46:19 -0500

    - Add missing changes to serial-uart header

--- kernel/device/serial-uart.h
+++ kernel/device/serial-uart.h
@@ -6,7 +6,9 @@ class uart_dev_t {
 public:
     virtual ~uart_dev_t() {}
 
-    virtual void init(ioport_t port, uint8_t port_irq, uint32_t baud) = 0;
+    virtual bool init(ioport_t port, uint8_t port_irq, uint32_t baud,
+                      uint8_t data_bits, char parity_type,
+                      uint8_t stop_bits) = 0;
 
     // Transfer up to size bytes, blocking until at least min is transferred
     // Return number of bytes transferred or negative value for error
@@ -15,5 +17,10 @@ public:
 
     virtual void route_irq(int) {}
 
-    static uart_dev_t *open(size_t id, bool simple);
+    // Simple early UART that does not allocate memory
+    static uart_dev_t *open(size_t id, bool simple, uint8_t data_bits, char parity_type, uint8_t stop_bits);
+
+    // Requires memory allocation
+    static uart_dev_t *open(uint16_t port, uint8_t irq,
+                            uint32_t baud, uint8_t data_bits, char parity_type, uint8_t stop_bits, bool polled);
 };
