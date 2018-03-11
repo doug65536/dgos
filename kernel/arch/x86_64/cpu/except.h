57506bf88888f3cf69416d05642335869974e806
Doug Gale <doug16k@gmail.com>
2018-03-11 13:18:09 -0400

    - Add __catch_code macro for getting exception code in catch block

--- kernel/arch/x86_64/cpu/except.h
+++ kernel/arch/x86_64/cpu/except.h
@@ -10,4 +10,6 @@ extern "C" int __exception_handler_invoke(int exception_code);
 __exception_jmp_buf_t *__exception_handler_add(void);
 
 #define __try if (__exception_setjmp(__exception_handler_add()) == 0)
+
 #define __catch if (__exception_handler_remove())
+#define __catch_code(__code) if (__code = __exception_handler_remove())
