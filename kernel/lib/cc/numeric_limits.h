8e90a042e4498e21cf999f3c9a126160ad58c541
Doug Gale <doug16k@gmail.com>
2018-05-06 04:31:39 -0400

    - Expand implementation of numeric_limits

--- kernel/lib/cc/numeric_limits.h
+++ kernel/lib/cc/numeric_limits.h
@@ -118,78 +118,150 @@ template<> class numeric_limits<bool>
         : public __basic_integral_numeric_limits<bool>
 {
     static constexpr bool traps = false;
+    static constexpr bool min() { return false; }
+    static constexpr bool max() { return true; }
 };
 
 template<> class numeric_limits<wchar_t>
         : public __basic_integral_numeric_limits<wchar_t>
 {
-};
-
-template<> class numeric_limits<char16_t>
-        : public __basic_integral_numeric_limits<char16_t>
-{
-};
-
-template<> class numeric_limits<char32_t>
-        : public __basic_integral_numeric_limits<char32_t>
-{
+public:
+    static constexpr wchar_t min() { return __WCHAR_MIN__; }
+    static constexpr wchar_t max() { return __WCHAR_MAX__; }
 };
 
 template<> class numeric_limits<char>
         : public __basic_integral_numeric_limits<char>
 {
+public:
+    static constexpr char min()
+    {
+        return -((unsigned char)~(unsigned char)0 >> 1) - 1;
+    }
+
+    static constexpr char max()
+    {
+        return ((unsigned char)~(unsigned char)0 >> 1);
+    }
 };
 
 template<> class numeric_limits<unsigned char>
         : public __basic_integral_numeric_limits<unsigned char>
 {
+public:
+    static constexpr unsigned char min() { return 0U; }
+    static constexpr unsigned char max() { return ~(unsigned char)0U; }
 };
 
 template<> class numeric_limits<signed char>
         : public __basic_integral_numeric_limits<signed char>
 {
+public:
+    static constexpr char min()
+    {
+        return -((unsigned char)~(unsigned char)0 >> 1) - 1;
+    }
+
+    static constexpr char max()
+    {
+        return ((unsigned char)~(unsigned char)0 >> 1);
+    }
 };
 
 template<> class numeric_limits<unsigned short>
         : public __basic_integral_numeric_limits<unsigned short>
 {
+public:
+    static constexpr unsigned short min() { return 0U; }
+    static constexpr unsigned short max() { return ~(unsigned short)0U; }
 };
 
 template<> class numeric_limits<signed short>
         : public __basic_integral_numeric_limits<signed short>
 {
+public:
+    static constexpr unsigned short min() { return 0U; }
+    static constexpr unsigned short max() { return __SHRT_MAX__; }
 };
 
 template<> class numeric_limits<unsigned int>
         : public __basic_integral_numeric_limits<unsigned int>
 {
+public:
+    static constexpr unsigned int min() { return 0U; }
+    static constexpr unsigned int max() { return ~0U; }
 };
 
 template<> class numeric_limits<signed int>
         : public __basic_integral_numeric_limits<signed int>
 {
+public:
+    static constexpr signed int min() { return -__INT_MAX__ - 1; }
+    static constexpr signed int max() { return __INT_MAX__; }
 };
 
 template<> class numeric_limits<unsigned long>
         : public __basic_integral_numeric_limits<unsigned long>
 {
+    static constexpr unsigned long min() { return 0UL; }
+    static constexpr unsigned long max() { return ~0UL; }
 };
 
 template<> class numeric_limits<signed long>
         : public __basic_integral_numeric_limits<signed long>
 {
+public:
+    static constexpr signed long min() { return 0L; }
+    static constexpr signed long max() { return __LONG_LONG_MAX__; }
 };
 
 template<> class numeric_limits<unsigned long long>
         : public __basic_integral_numeric_limits<unsigned long long>
 {
+public:
+    static constexpr unsigned long long min() { return 0; }
+    static constexpr unsigned long long max() { return ~0ULL; }
 };
 
 template<> class numeric_limits<signed long long>
         : public __basic_integral_numeric_limits<signed long long>
 {
+public:
+    static constexpr signed long long min() { return -__LONG_LONG_MAX__ - 1; }
+    static constexpr signed long long max() { return __LONG_LONG_MAX__; }
 };
 
+template<> class numeric_limits<char16_t>
+        : public __basic_integral_numeric_limits<char16_t>
+{
+public:
+    static constexpr char16_t min()
+    {
+        return numeric_limits<__CHAR16_TYPE__>::min();
+    }
+
+    static constexpr char16_t max()
+    {
+        return numeric_limits<__CHAR16_TYPE__>::max();
+    }
+};
+
+template<> class numeric_limits<char32_t>
+        : public __basic_integral_numeric_limits<char32_t>
+{
+public:
+    static constexpr char16_t min()
+    {
+        return numeric_limits<__CHAR16_TYPE__>::min();
+    }
+
+    static constexpr char16_t max()
+    {
+        return numeric_limits<__CHAR16_TYPE__>::max();
+    }
+};
+
+#ifndef __DGOS_KERNEL__
 template<> class numeric_limits<float>
         : public __basic_fp_numeric_limits
 {
@@ -200,6 +272,8 @@ template<> class numeric_limits<float>
     static constexpr int min_exponent10 = __FLT_MIN_10_EXP__;
     static constexpr int max_exponent = __FLT_MAX_EXP__;
     static constexpr int max_exponent10 = __FLT_MAX_10_EXP__;
+    static constexpr float min() { return __FLT32_MIN__; }
+    static constexpr float max() { return __FLT32_MAX__; }
 };
 
 template<> class numeric_limits<double>
@@ -212,6 +286,8 @@ template<> class numeric_limits<double>
     static constexpr int min_exponent10 = __DBL_MIN_10_EXP__;
     static constexpr int max_exponent = __DBL_MAX_EXP__;
     static constexpr int max_exponent10 = __DBL_MAX_10_EXP__;
+    static constexpr double min() { return __FLT64_MIN__; }
+    static constexpr double max() { return __FLT64_MAX__; }
 };
 
 template<> class numeric_limits<long double>
@@ -224,4 +300,7 @@ template<> class numeric_limits<long double>
     static constexpr int min_exponent10 = __LDBL_MIN_10_EXP__;
     static constexpr int max_exponent = __LDBL_MAX_EXP__;
     static constexpr int max_exponent10 = __LDBL_MAX_10_EXP__;
+    static constexpr long double min() { return __LDBL_MIN__; }
+    static constexpr long double max() { return __LDBL_MAX__; }
 };
+#endif
