6031076efb85a2921a38f1cc4589136c355fb08b
Doug Gale <doug16k@gmail.com>
2018-07-10 23:41:26 -0400

    bootefi: Correctly query the boot filesystem from the image handle - Implement boot drive serial number in EFI using block I/O API

--- boot/bootefi.cc
+++ boot/bootefi.cc
@@ -1,4 +1,5 @@
 #include "bootefi.h"
+#include "string.h"
 #include "screen.h"
 #include "debug.h"
 #include "assert.h"
@@ -14,11 +15,6 @@ EFI_SYSTEM_TABLE *efi_systab;
 #if 1
 #include "fs.h"
 
-int x _used;
-
-static UINTN efi_num_filesys_handles;
-static EFI_HANDLE *efi_filesys_handles;
-
 static EFI_BLOCK_IO_PROTOCOL *efi_blk_io;
 static EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *efi_simple_filesystem;
 static EFI_FILE *efi_root_dir;
@@ -29,11 +25,11 @@ static EFI_GUID const efi_simple_file_system_protocol_guid = {
     }
 };
 
-//static EFI_GUID const efi_loaded_image_proto = {
-//    0x5B1B31A1, 0x9562, 0x11d2, {
-//        0x8E,0x3F,0x00,0xA0,0xC9,0x69,0x72,0x3B
-//    }
-//};
+static EFI_GUID const efi_loaded_image_protocol_guid = {
+    0x5B1B31A1, 0x9562, 0x11d2, {
+        0x8E,0x3F,0x00,0xA0,0xC9,0x69,0x72,0x3B
+    }
+};
 
 static EFI_GUID const efi_block_io_protocol_guid = {
     0x964e5b21, 0x6459, 0x11d2, {
@@ -139,7 +135,24 @@ static int efi_pread(int file, void *buf, size_t bytes, off_t ofs)
 
 static uint64_t efi_boot_drv_serial()
 {
-    return 0;
+    EFI_STATUS status;
+
+    char *buffer = (char*)malloc(efi_blk_io->Media->BlockSize);
+
+    status = efi_blk_io->ReadBlocks(efi_blk_io, efi_blk_io->Media->MediaId,
+                                    0, efi_blk_io->Media->BlockSize, buffer);
+
+    uint64_t serial = 0;
+    if (!EFI_ERROR(status)) {
+        // BPB serial is 32 bit
+        memcpy(&serial, buffer + 0x43, sizeof(uint32_t));
+    } else {
+        serial = -1;
+    }
+
+    free(buffer);
+
+    return serial;
 }
 
 extern "C" void halt(tchar const *s)
@@ -152,26 +165,24 @@ _constructor(ctor_fs) void register_efi_fs()
 {
     EFI_STATUS status;
 
-    // Get a handle to this executable, and get the
-    // simple_file_system_protocol_guid handle to this executable
-    status = efi_systab->BootServices->LocateHandleBuffer(
-                ByProtocol,
-                &efi_simple_file_system_protocol_guid,
-                nullptr,
-                &efi_num_filesys_handles,
-                &efi_filesys_handles);
+    EFI_LOADED_IMAGE_PROTOCOL *efi_loaded_image;
+
+    status = efi_systab->BootServices->HandleProtocol(
+                efi_image_handle,
+                &efi_loaded_image_protocol_guid,
+                (VOID**)&efi_loaded_image);
 
     if (EFI_ERROR(status))
-        halt(TSTR "LocateHandleBuffer failed");
+        halt(TSTR "HandleProtocol LOADED_IMAGE_PROTOCOL failed");
 
     // Get the vtbl for the simple_filesystem_protocol of this executable
     status = efi_systab->BootServices->HandleProtocol(
-                efi_filesys_handles[0],
-            &efi_simple_file_system_protocol_guid,
-            (VOID**)&efi_simple_filesystem);
+                efi_loaded_image->DeviceHandle,
+                &efi_simple_file_system_protocol_guid,
+                (VOID**)&efi_simple_filesystem);
 
     if (EFI_ERROR(status))
-        halt(TSTR "HandleProtocol for simple_file_system_protocol failed");
+        halt(TSTR "HandleProtocol SIMPLE_FILE_SYSTEM_PROTOCOL failed");
 
     // Open the root directory of the volume containing this executable
     status = efi_simple_filesystem->OpenVolume(
@@ -181,7 +192,7 @@ _constructor(ctor_fs) void register_efi_fs()
         halt(TSTR "OpenVolume for boot partition failed");
 
     status = efi_systab->BootServices->HandleProtocol(
-            efi_filesys_handles[0],
+            efi_loaded_image->DeviceHandle,
             &efi_block_io_protocol_guid,
             (VOID**)&efi_blk_io);
 
