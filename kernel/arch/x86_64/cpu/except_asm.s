65800852c36cd5192dfa1e72f906abc226cc4b1f
Doug Gale <doug16k@gmail.com>
2018-07-25 00:40:26 -0400

    longjmp: make setjmp and longjmp more consistent

--- kernel/arch/x86_64/cpu/except_asm.s
+++ kernel/arch/x86_64/cpu/except_asm.s
@@ -3,40 +3,48 @@
 .global __exception_setjmp
 .hidden __exception_setjmp
 __exception_setjmp:
-	# Save return address
-	mov (%rsp),%rax
-	mov %rax,(%rdi)
-
-	# Save callers stack pointer
-	lea 8(%rsp),%rax
-	mov %rax,1*8(%rdi)
-
 	# Save call preserved registers
-	mov %rbx,2*8(%rdi)
-	mov %rbp,3*8(%rdi)
-	mov %r12,4*8(%rdi)
-	mov %r13,5*8(%rdi)
-	mov %r14,6*8(%rdi)
-	mov %r15,7*8(%rdi)
+	movq %rbx,2*8(%rdi)
+	movq %rbp,3*8(%rdi)
+	movq %r12,4*8(%rdi)
+	movq %r13,5*8(%rdi)
+	movq %r14,6*8(%rdi)
+	movq %r15,7*8(%rdi)
+
+	# Save flags
 	pushfq
 	popq 8*8(%rdi)
-	xor %eax,%eax
+
+	# Save return address
+	movq (%rsp),%rax
+	movq %rax,(%rdi)
+
+	# Save callers stack pointer
+	leaq 8(%rsp),%rax
+	movq %rax,1*8(%rdi)
+
+	# Return value is zero
+	xorl %eax,%eax
 	ret
 
 .global __exception_longjmp
 .hidden __exception_longjmp
 __exception_longjmp:
 	# Restore call preserved registers
-	mov 2*8(%rdi),%rbx
-	mov 3*8(%rdi),%rbp
-	mov 4*8(%rdi),%r12
-	mov 5*8(%rdi),%r13
-	mov 6*8(%rdi),%r14
-	mov 7*8(%rdi),%r15
+	movq 2*8(%rdi),%rbx
+	movq 3*8(%rdi),%rbp
+	movq 4*8(%rdi),%r12
+	movq 5*8(%rdi),%r13
+	movq 6*8(%rdi),%r14
+	movq 7*8(%rdi),%r15
+
+	# Restore flags
 	pushq 8*8(%rdi)
 	popfq
-	# Return value is second parameter
-	mov %rsi,%rax
-	# Restore stack pointer and "return" from setjmp again
-	mov 1*8(%rdi),%rsp
-	jmp *(%rdi)
+
+	# Restore stack pointer
+	movq 1*8(%rdi),%rsp
+
+	# Return value is second parameter and "return" from setjmp again
+	movq %rsi,%rax
+	jmpq *(%rdi)
