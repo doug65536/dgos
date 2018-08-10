9cb9d96e1f21e70c68762314990489ed116aa353
Doug Gale <doug16k@gmail.com>
2018-08-10 19:39:27 -0400

    Remove old libc

--- user/libc/src/memcpy.cc
+++
@@ -1,9 +0,0 @@
-
-void *memcpy(void *d, void *s, unsigned long size)
-{
-	char *sp = (char*)s;
-	char *dp = (char*)d;
-    for (unsigned long i = 0; i < size; ++i)
-        dp[i] = sp[i];
-    return d;
-}
