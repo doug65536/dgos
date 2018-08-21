5f295bf7b1fcc47c9343c0772a877513447b772c
Doug Gale <doug16k@gmail.com>
2018-08-21 14:27:37 -0400

    callout: Declare const

--- kernel/lib/callout.cc
+++ kernel/lib/callout.cc
@@ -1,11 +1,11 @@
 #include "callout.h"
 
-extern callout_t ___callout_array_st[];
-extern callout_t ___callout_array_en[];
+extern callout_t const ___callout_array_st[];
+extern callout_t const ___callout_array_en[];
 
 size_t callout_call(callout_type_t type)
 {
-    for (callout_t *callout = ___callout_array_st;
+    for (callout_t const *callout = ___callout_array_st;
          callout < ___callout_array_en; ++callout) {
         if (callout->type == type)
             callout->fn(callout->userarg);
