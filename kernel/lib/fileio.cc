22c5187300d03e84035af30b94888a46e21ebecf
Doug Gale <doug16k@gmail.com>
2018-08-22 09:52:19 -0400

    fileio: Fix several error handling bugs and file descriptor leaks

--- kernel/lib/fileio.cc
+++ kernel/lib/fileio.cc
@@ -104,14 +104,19 @@ int file_open(char const *path, int flags, mode_t mode)
 {
     fs_base_t *fs = file_fs_from_path(path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     filetab_t *fh = file_new_filetab();
 
+    if (unlikely(!fh))
+        return -int(errno_t::ENFILE);
+
     int status = fs->open(&fh->fi, path, flags, mode);
-    if (status < 0)
+    if (unlikely(status < 0)) {
+        file_del_filetab(fh);
         return status;
+    }
 
     fh->fs = fs;
     fh->pos = 0;
@@ -122,9 +127,8 @@ int file_open(char const *path, int flags, mode_t mode)
 int file_close(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-
     if (unlikely(!fh))
-        return -1;
+        return -int(errno_t::EBADF);
 
     if (file_del_filetab(fh)) {
         fh->fs->release(fh->fi);
@@ -137,8 +141,8 @@ int file_close(int id)
 ssize_t file_pread(int id, void *buf, size_t bytes, off_t ofs)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->read(fh->fi, (char*)buf, bytes, ofs);
 }
@@ -146,8 +150,8 @@ ssize_t file_pread(int id, void *buf, size_t bytes, off_t ofs)
 ssize_t file_pwrite(int id, void const *buf, size_t bytes, off_t ofs)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->write(fh->fi, (char*)buf, bytes, ofs);
 }
@@ -155,8 +159,8 @@ ssize_t file_pwrite(int id, void const *buf, size_t bytes, off_t ofs)
 int file_syncfs(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->flush(fh->fi);
 }
@@ -164,33 +168,35 @@ int file_syncfs(int id)
 off_t file_seek(int id, off_t ofs, int whence)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
+    int status;
     fs_stat_t st;
 
     switch (whence) {
     case SEEK_SET:
-        if (ofs < 0)
-            return -1;
+        if (unlikely(ofs < 0))
+            return -int(errno_t::EINVAL);
 
         fh->pos = ofs;
         break;
 
     case SEEK_CUR:
-        if (fh->pos + ofs < 0)
-            return -1;
+        if (unlikely(fh->pos + ofs < 0))
+            return -int(errno_t::EINVAL);
 
         fh->pos += ofs;
         break;
 
     case SEEK_HOLE: // fall through
     case SEEK_END:
-        if (fh->fs->fstat(fh->fi, &st) < 0)
-            return -1;
+        status = fh->fs->fstat(fh->fi, &st);
+        if (unlikely(status < 0))
+            return status;
 
         if (st.st_size + ofs < 0)
-            return -1;
+            return -int(errno_t::EINVAL);
 
         fh->pos = st.st_size + ofs;
         break;
@@ -200,7 +206,8 @@ off_t file_seek(int id, off_t ofs, int whence)
         break;
 
     default:
-        return -1;
+        return -int(errno_t::EINVAL);
+
     }
 
     return fh->pos;
@@ -209,8 +216,8 @@ off_t file_seek(int id, off_t ofs, int whence)
 int file_ftruncate(int id, off_t size)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->ftruncate(fh->fi, size);
 }
@@ -218,8 +225,8 @@ int file_ftruncate(int id, off_t size)
 int file_ioctl(int id, int cmd, void* arg, unsigned int flags, void* data)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->ioctl(fh->fi, cmd, arg, flags, data);
 }
@@ -227,8 +234,8 @@ int file_ioctl(int id, int cmd, void* arg, unsigned int flags, void* data)
 ssize_t file_read(int id, void *buf, size_t bytes)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     ssize_t size = fh->fs->read(fh->fi, (char*)buf, bytes, fh->pos);
     if (size >= 0)
@@ -240,8 +247,8 @@ ssize_t file_read(int id, void *buf, size_t bytes)
 ssize_t file_write(int id, void const *buf, size_t bytes)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     ssize_t size = fh->fs->write(fh->fi, (char*)buf, bytes, fh->pos);
     if (size >= 0)
@@ -253,8 +260,8 @@ ssize_t file_write(int id, void const *buf, size_t bytes)
 int file_fsync(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->fsync(fh->fi, 0);
 }
@@ -262,8 +269,8 @@ int file_fsync(int id)
 int file_fdatasync(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->fsync(fh->fi, 1);
 }
@@ -272,15 +279,21 @@ int file_opendir(char const *path)
 {
     fs_base_t *fs = file_fs_from_path(path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     filetab_t *fh = file_new_filetab();
+
+    if (unlikely(!fh))
+        return -int(errno_t::ENFILE);
+
     fh->fs = fs;
 
     int status = fh->fs->opendir(&fh->fi, path);
-    if (status < 0)
-        return -1;
+    if (unlikely(status < 0)) {
+        file_del_filetab(fh);
+        return status;
+    }
 
     fh->fs = fs;
     //fh->path = strdup(path);
@@ -293,14 +306,14 @@ int file_opendir(char const *path)
 ssize_t file_readdir_r(int id, dirent_t *buf, dirent_t **result)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     ssize_t size = fh->fs->readdir(fh->fi, buf, fh->pos);
 
     *result = nullptr;
     if (size < 0)
-        return -1;
+        return -int(errno_t::EINVAL);
 
     fh->pos += size;
     *result = buf;
@@ -310,8 +323,8 @@ ssize_t file_readdir_r(int id, dirent_t *buf, dirent_t **result)
 off_t file_telldir(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->pos;
 }
@@ -319,8 +332,8 @@ off_t file_telldir(int id)
 off_t file_seekdir(int id, off_t ofs)
 {
     filetab_t *fh = file_fh_from_id(id);
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     fh->pos = ofs;
     return 0;
@@ -329,9 +342,8 @@ off_t file_seekdir(int id, off_t ofs)
 int file_closedir(int id)
 {
     filetab_t *fh = file_fh_from_id(id);
-
-    if (!fh)
-        return -1;
+    if (unlikely(!fh))
+        return -int(errno_t::EBADF);
 
     return fh->fs->releasedir(fh->fi);
 }
@@ -340,8 +352,8 @@ int file_mkdir(char const *path, mode_t mode)
 {
     fs_base_t *fs = file_fs_from_path(path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     return fs->mkdir(path, mode);
 }
@@ -350,8 +362,8 @@ int file_rmdir(char const *path)
 {
     fs_base_t *fs = file_fs_from_path(path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     return fs->rmdir(path);
 }
@@ -360,8 +372,8 @@ int file_rename(char const *old_path, char const *new_path)
 {
     fs_base_t *fs = file_fs_from_path(old_path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     return fs->rename(old_path, new_path);
 }
@@ -370,8 +382,8 @@ int file_unlink(char const *path)
 {
     fs_base_t *fs = file_fs_from_path(path);
 
-    if (!fs)
-        return -1;
+    if (unlikely(!fs))
+        return -int(errno_t::ENOENT);
 
     return fs->unlink(path);
 }
