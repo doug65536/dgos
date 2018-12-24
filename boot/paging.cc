2c1d84d09b5e90ee4ed3c07de3f056691eab6a35
Doug Gale <doug16k@gmail.com>
2018-12-23 22:36:18 -0330

    boot/paging: try harder to use large pages in every case possible   when mapping physical memory

--- boot/paging.cc
+++ boot/paging.cc
@@ -227,9 +227,190 @@ int paging_iovec(iovec_t **ret, uint64_t vaddr,
     return count;
 }
 
+static inline constexpr uint64_t low_bits(uint64_t value, uint8_t log2n)
+{
+    return value & ((1 << log2n) - 1);
+}
+
+static inline constexpr uint64_t round_up(uint64_t value, uint8_t log2n)
+{
+    return (value + ((1 << log2n) - 1)) & -(1 << log2n);
+}
+
+static inline constexpr uint64_t round_dn(uint64_t value, uint8_t log2n)
+{
+    return value & -(1 << log2n);
+}
+
+void paging_map_physical_impl(uint64_t phys_addr, uint64_t linear_base,
+                              uint64_t length, uint64_t pte_flags);
+
 // Incoming pte_flags should be as if 4KB page (bit 7 is PAT bit)
 void paging_map_physical(uint64_t phys_addr, uint64_t linear_base,
                          uint64_t length, uint64_t pte_flags)
+{
+    if (unlikely(low_bits(phys_addr, 12) != low_bits(linear_base, 12)))
+    {
+        assert_msg(false,
+                   TSTR "Impossible linear->physical mapping,"
+                   TSTR " bits 11:0 of the physical and linear address"
+                   TSTR " must be equal");
+        return;
+    }
+
+    // -------------------------========---------------------------  //
+    //                                                               //
+    //                          4KB Only                             //
+    //                                                               //
+    //   <- phys_addr                        phys_addr + length ->   //
+    //   <- linear_base                                            B //
+    //                                                             C //
+    //                                                             D //
+    //                                                             E //
+    // A <------------------------ r ----------------------------> F //
+    // |                                                           | //
+    // | L                                                         | //
+    // | |                                                         | //
+    // ↓ ↓                                                         ↓ //
+    // +-----------------------------------------------------------+ //
+    // |                        4K pg ...                          | //
+    // +-----------------------------------------------------------+ //
+    // ↑                                                           ↑ //
+    // |                                                           | //
+    // 4K                                                         4K //
+    // \------------------------ alignment ------------------------/ //
+    //                                                               //
+    //                                                               //
+    // --------------------------=======---------------------------  //
+    //                                                               //
+    //                           4KB/2MB                             //
+    //                                                               //
+    // r_up(A,21) -↘                                   ↙- r_dn(F,21) //
+    //                                                 C             //
+    //             ↙                                   D             //
+    // A <-- r --> B <------------- s ---------------> E <-- v --> F //
+    // |           |                                   |           | //
+    // | L         |                                   |           | //
+    // | |         |                                   |           | //
+    // ↓ ↓         ↓                                   ↓           ↓ //
+    // +-----------+-----------------------------------+-----------+ //
+    // | 4K pg ... |             2M pg ...             | 4K pg ... | //
+    // +-----------+-----------------------------------+-----------+ //
+    // ↑           ↑                                   ↑           ↑ //
+    // |           |                                   |           | //
+    // 4K          2M                                  2M         4K //
+    // \------------------------ alignment ------------------------/ //
+    //                                                               //
+    //                                                               //
+    // ------------------------===========-------------------------  //
+    //                                                               //
+    //                         4KB/2MB/1GB                           //
+    //                                                               //
+    //                                                               //
+    //              r_up(B,30)-↘           ↙-r_dn(E,30)              //
+    //                         |           |                         //
+    // A <-- r --> B <-- s --> C <-- t --> D <-- u --> E <-- v --> F //
+    // |           |           |           |           |           | //
+    // | L         |           |           |           |           | //
+    // | |         |           |           |           |           | //
+    // ↓ ↓         ↓           ↓           ↓           ↓           ↓ //
+    // +-----------+-----------+-----------+-----------+-----------+ //
+    // | 4K pgs... | 2M pgs... | 1G pgs... | 2M pgs... | 4K pgs... | //
+    // +-----------+-----------+-----------+-----------+-----------+ //
+    // ↑           ↑           ↑           ↑           ↑           ↑ //
+    // |           |           |           |           |           | //
+    // 4K          2M          1G          1G          2M         4K //
+    // \------------------------ alignment ------------------------/ //
+    //                                                               //
+    //                                                               //
+    // A is phys_addr rounded down to a 4KB boundary                 //
+    // F is phys_addr + length rounded up to a 4KB boundary          //
+    // B is A rounded up to a 2MB boundary                           //
+    // C is B rounded up to a 1GB boundary                           //
+    // E is F rounded down to a 1MB boundary                         //
+    // D is E rounded down to a 1GB boundary                         //
+    //                                                               //
+    // r = B - A (4KB pages)                                         //
+    // s = C - B (2MB pages)                                         //
+    // t = D - C (1GB pages)                                         //
+    // u = E - D (2MB pages)                                         //
+    // v = F - E (4KB pages)                                         //
+    //                                                               //
+    // Usually, several regions are empty. When alignment permits,   //
+    // only the largest page sizes will be used. It starts as a      //
+    // single run of 4KB pages, then a run of 2MB regions is carved  //
+    // out of it, eliminating some or all 4KB runs, then a run of    //
+    // 1GB pages is carved out of it, eliminating some or all of the //
+    // 2MB runs                                                      //
+
+    // It's impossible to use a 1GB mapping if the low 30 bits of the
+    // physical address and the linear address are not equal
+    bool can_use_1G = low_bits(phys_addr, 30) == low_bits(linear_base, 30);
+
+    // It's impossible to use a 2MB mapping if the low 21 bits of the
+    // physical address and the linear address are not equal
+    bool can_use_2M = low_bits(phys_addr, 21) == low_bits(linear_base, 21);
+
+    uint64_t phys_end = phys_addr + length;
+
+    uint64_t A, B, C, D, E, F, X, Y;
+
+    // Start with a simple run of 4KB pages
+    A = round_dn(phys_addr, 12);
+    F = round_up(phys_end, 12);
+    B = C = D = E = F;
+
+    // Compute 2MB rounded boundaries for B,C/D/E
+    X = round_up(A, 21);
+    Y = round_dn(F, 21);
+
+    if (X < Y && can_use_2M)
+    {
+        B = X;
+        C = D = E = Y;
+    }
+
+    // Compute 1GB rounded boundaries for C,D
+    X = round_up(X, 30);
+    Y = round_dn(Y, 30);
+
+    if (X < Y && can_use_1G)
+    {
+        C = X;
+        D = Y;
+    }
+
+    int64_t r = B - A;  // 4KB
+    int64_t s = C - B;  // 2MB
+    int64_t t = D - C;  // 1GB
+    int64_t u = E - D;  // 2MB
+    int64_t v = F - E;  // 4KB
+
+    int64_t phys_to_virt = linear_base - phys_addr;
+
+    // 4KB page region
+    if (r > 0)
+        paging_map_physical_impl(A, A + phys_to_virt, r, pte_flags);
+
+    // 2MB page region
+    if (s > 0)
+        paging_map_physical_impl(B, B + phys_to_virt, s, pte_flags);
+
+    // 1GB page region
+    if (t > 0)
+        paging_map_physical_impl(C, C + phys_to_virt, t, pte_flags);
+
+    // 2MB page region
+    if (u > 0)
+        paging_map_physical_impl(D, D + phys_to_virt, u, pte_flags);
+
+    // 4KB page region
+    if (v > 0)
+        paging_map_physical_impl(E, E + phys_to_virt, v, pte_flags);
+}
+
+void paging_map_physical_impl(uint64_t phys_addr, uint64_t linear_base,
+                              uint64_t length, uint64_t pte_flags)
 {
     // Make sure the flags don't set any address bits
     assert((pte_flags & PTE_ADDR) == 0);
