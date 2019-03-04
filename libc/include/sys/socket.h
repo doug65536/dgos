1675b6948aeb152ba62e2d2d6d04345421e9880a
Doug Gale <doug16k@gmail.com>
2019-03-04 16:27:02 -0330

    socket: add some user API declarations

--- libc/include/sys/socket.h
+++ libc/include/sys/socket.h
@@ -15,11 +15,18 @@ __BEGIN_DECLS
 #define IPPROTO_UDP 17
 
 typedef uint32_t socklen_t;
+typedef uint32_t sa_family_t;
+
+struct sockaddr {
+    sa_family_t sa_family;
+    char        sa_data[14];
+};
 
 int socket(int __domain, int __type, int __protocol);
 int send(int __fd, void const *__buf, int __len, int __flags);
 int recv(int __fd, void *__buf, int __len, int __flags);
 
-int accept(int __sockfd, struct sockaddr *__addr, socklen_t *__addrlen);
+int accept(int __fd, struct sockaddr *__addr, socklen_t *__addrlen);
+int bind(int __fd, struct sockaddr *addr, socklen_t addrlen);
 
 __END_DECLS
