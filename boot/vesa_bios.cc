d2f8d15708d3ffbec90df5b48c75d3be8f97b0de
Doug Gale <doug16k@gmail.com>
2019-11-15 21:40:19 -0500

    Make a couple of function static

--- boot/vesa_bios.cc
+++ boot/vesa_bios.cc
@@ -63,10 +63,10 @@ bool vbe_set_mode(vbe_selected_mode_t& mode)
     return (regs.eax & 0xFFFF) == 0x4F;
 }
 
-void selected_from_vbeinfo(vbe_mode_info_t * restrict mode_info,
-                           vbe_info_t * restrict info,
-                           vbe_selected_mode_t * restrict sel,
-                           uint16_t mode_num)
+static void selected_from_vbeinfo(vbe_mode_info_t * restrict mode_info,
+                                  vbe_info_t * restrict info,
+                                  vbe_selected_mode_t * restrict sel,
+                                  uint16_t mode_num)
 {
     sel->mode_num = mode_num;
     sel->width = mode_info->res_x;
@@ -87,7 +87,7 @@ void selected_from_vbeinfo(vbe_mode_info_t * restrict mode_info,
     memset(sel->reserved, 0, sizeof(sel->reserved));
 }
 
-bool vbe_is_mode_usable(vbe_mode_info_t const *mode)
+static bool vbe_is_mode_usable(vbe_mode_info_t const *mode)
 {
     return mode->planes == 1 &&
             (mode->phys_base_ptr ||
@@ -145,83 +145,3 @@ vbe_mode_list_t const& vbe_enumerate_modes()
 
     return mode_list;
 }
-
-//vbe_selected_mode_t *vbe_select_mode(
-//        uint32_t width, uint32_t height, bool verbose)
-//{
-//    vbe_info_t *info = new (std::nothrow) vbe_info_t;
-//    vbe_mode_info_t *mode_info = new vbe_mode_info_t;
-
-//    vbe_selected_mode_t *sel = new (std::nothrow) vbe_selected_mode_t{};
-
-//    uint16_t mode;
-//    uint16_t done = 0;
-//    if (vbe_detect(info)) {
-//        VESA_TRACE("VBE Memory %dMB\n", info->mem_size_64k >> 4);
-
-//        uint16_t *modes = info->mode_list_ptr;
-
-//        while (!done) {
-//            // Get mode number
-//            mode = *modes++;
-
-//            if (mode == 0xFFFF)
-//                break;
-
-//            // Get mode information
-//            if (vbe_mode_info(mode_info, mode)) {
-//                // Ignore palette modes
-//                if (!mode_info->mask_size_r &&
-//                        !mode_info->mask_size_g &&
-//                        !mode_info->mask_size_b &&
-//                        !mode_info->mask_size_rsvd)
-//                    continue;
-
-//                aspect_ratio(&sel->aspect_n, &sel->aspect_d,
-//                             mode_info->res_x,
-//                             mode_info->res_y);
-
-//                if (verbose) {
-//                    VESA_TRACE("vbe mode %u w=%u h=%u"
-//                               " %d:%d:%d:%d phys_addr=%" PRIx32 " %d:%d",
-//                               mode,
-//                               mode_info->res_x,
-//                               mode_info->res_y,
-//                               mode_info->mask_size_r,
-//                               mode_info->mask_size_g,
-//                               mode_info->mask_size_b,
-//                               mode_info->mask_size_rsvd,
-//                               mode_info->phys_base_ptr,
-//                               sel->aspect_n, sel->aspect_d);
-//                }
-
-//                if (mode_info->res_x == width &&
-//                        mode_info->res_y == height &&
-//                        mode_info->bpp == 32) {
-//                    selected_from_vbeinfo(mode_info, info, sel, mode);
-//                    done = 1;
-//                    vbe_set_mode(*sel);
-//                    break;
-//                }
-//            }
-//        }
-//    }
-
-//    void *kernel_data = nullptr;
-
-//    if (done) {
-//        kernel_data = malloc(sizeof(sel));
-//        memcpy(kernel_data, &sel, sizeof(sel));
-
-//        uint64_t pte_flags = PTE_PRESENT | PTE_WRITABLE |
-//                (-cpu_has_global_pages() & PTE_GLOBAL);
-
-//        paging_map_physical(sel->framebuffer_addr, sel->framebuffer_addr,
-//                            sel->framebuffer_bytes, pte_flags);
-//    }
-
-//    delete mode_info;
-//    delete info;
-
-//    return (vbe_selected_mode_t*)kernel_data;
-//}
