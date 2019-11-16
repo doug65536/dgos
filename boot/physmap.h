dc51688ac4fe5a211928c22fc6ef657503b34509
Doug Gale <doug16k@gmail.com>
2019-11-15 21:41:54 -0500

    Add API to get top of memory map

--- boot/physmap.h
+++ boot/physmap.h
@@ -4,6 +4,7 @@
 #include "physmem.h"
 #include "paging.h"
 
-physmem_range_t *physmap_get(int *ret_count);
+physmem_range_t *physmap_get(size_t *ret_count);
 int physmap_insert(physmem_range_t const& entry);
 int physmap_take_range(uint64_t base, uint64_t size, uint32_t type);
+uint64_t physmap_top_addr();
