11ec3cf3cac2f8e65e86b0b178611b72e7978670
Doug Gale <doug16k@gmail.com>
2020-06-30 09:25:40 -0400

    Fix standard stream fileno

--- libc/src/stdio/stdin_out_err.cc
+++ libc/src/stdio/stdin_out_err.cc
@@ -3,8 +3,8 @@
 #include "bits/cfile.h"
 
 static _FILE stdin_bits{STDIN_FILENO};
-static _FILE stdout_bits{STDIN_FILENO};
-static _FILE stderr_bits{STDIN_FILENO};
+static _FILE stdout_bits{STDOUT_FILENO};
+static _FILE stderr_bits{STDERR_FILENO};
 
 FILE *stdin = &stdin_bits;
 FILE *stdout = &stdout_bits;
