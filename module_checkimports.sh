072ee07b3f3555a6944d05747aca251da00903f6
Doug Gale <doug16k@gmail.com>
2020-07-10 01:19:26 -0400

    Update module_checkimports.sh to follow NEEDED dependencies and include their exports

--- module_checkimports.sh
+++ module_checkimports.sh
@@ -11,6 +11,10 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
+if [[ -z "$OBJDUMP" ]]; then
+    OBJDUMP=objdump
+fi
+
 modules="$@"
 
 scriptroot="$(cd "$(dirname "$0")" && pwd)"
@@ -23,12 +27,40 @@ done < <("$scriptroot/kernel_exports.sh" $objdump_args)
 
 echo Found ${#kernel_syms[@]} kernel symbols...
 
+declare -a needed
+declare -A syms
+
 failed=
 for module in $modules; do
     echo Checking $module...
+
+    needed=()
+
+    # Get the needed modules from the dynamic section using objdump
+    while IFS= read -r line; do
+        needed+=("$line")
+    done < <("$OBJDUMP" -p "$module" |
+        grep -oP '(?<=NEEDED)\s+\S+\.km' |
+        grep -oP '\S.*')
+
+    syms=()
+
+    for sym in "${!kernel_syms[@]}"; do
+        syms+=([$sym]=1)
+    done
+
+    for need in "${needed[@]}"; do
+        echo ...needs $need
+        while IFS= read -r line; do
+            syms+=([$line]=1)
+        done < <("$scriptroot/kernel_exports.sh" $objdump_args "$need")
+    done
+
+    #echo ${#syms[@]} symbols available
+
     msg=
     while IFS= read -r line; do
-        if [[ -z "${kernel_syms[$line]}" ]]; then
+        if [[ -z "${syms[$line]}" ]]; then
             if [[ -z "$msg" ]]; then
                 echo error: Module $module failed
                 msg=1
