8cb8a52af56ca85dd6b57f59ba7ab48806a69aea
Doug Gale <doug16k@gmail.com>
2020-07-11 22:57:25 -0400

    Formatting

--- user/bin/ls/ls.cc
+++ user/bin/ls/ls.cc
@@ -5,12 +5,17 @@
 int main(int argc, char const **argv)
 {
     DIR *dir = opendir(".");
+
     if (!dir)
         return 1;
+
     struct dirent *de;
+
     while (de = readdir(dir)) {
         printf("%s\n", de->d_name);
     }
+
     closedir(dir);
+
     return 0;
 }
