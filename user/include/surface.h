6c9e24500922e5c184069a137a9962e8b57cca1d
Doug Gale <doug16k@gmail.com>
2020-07-31 19:46:44 -0400

    change surface names

--- user/include/surface.h
+++ user/include/surface.h
@@ -6,15 +6,15 @@
 struct surface_t {
     int32_t width;
     int32_t height;
-    void *reserved;
+    void *reserved[1];
 };
 
 void surface_free(surface_t *pp);
 void surface_autofree(surface_t **pp);
 
-static inline uint32_t const *surface_pixels(surface_t const *img)
+static inline uint32_t const *surface_pixels(surface_t const *s)
 {
-    return (uint32_t const*)(img + 1);
+    return (uint32_t const*)(s + 1);
 }
 
-#define autofree_png __attribute__((cleanup(png_free)))
+#define autofree_png __attribute__((__cleanup__(png_free)))
