bb8e73d8a2fd5eb4e1b829ae320e12539147b0c3
Doug Gale <doug16k@gmail.com>
2020-08-27 21:35:50 -0400

    Make symbol table output rows unique

--- gensymtab.sh
+++ gensymtab.sh
@@ -24,14 +24,14 @@ if [[ $type == "e" ]]; then
 		grep -vP '^[0-9a-f]+\s.....d' | \
 		grep -vP '\*ABS\*' |
 		sed -E 's/^([0-9a-f]+)\s.{7}\s\S+\s+\S+\s+(.hidden )?(.*)/\1 \3/' | \
-		$SORT > "$output"
+		$SORT -u > "$output"
 elif [[ $type == "p" ]]; then
 	$OBJDUMP --syms --demangle --wide "$input" | \
 		grep -P '0x[0-9a-f]{8,16}\s' | \
 		grep -v '^0000000000000000' | \
 		grep -vP '\(sec\s+-\d+\)' | \
 		grep -oP '(?<= 0x)[0-9a-f]{8,16} .*$' |
-		$SORT > "$output"
+		$SORT -u > "$output"
 else
 	printf "Invalid type argument, must be p for PE or e for ELF\n"
 	exit 1
