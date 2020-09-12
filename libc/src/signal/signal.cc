6347f7ff17b0703b73c037ff4199c1a5df39bbf0
Doug Gale <doug16k@gmail.com>
2020-09-12 17:18:40 -0400

    signal wrapper (partial)

--- libc/src/signal/signal.cc
+++ libc/src/signal/signal.cc
@@ -1,2 +1,19 @@
 #include <signal.h>
+#include <errno.h>
 
+void (*signal(int sig, void (*handler)(int)))(int)
+{
+//    sigaction act;
+//    sigaction old_act;
+//    act.sa_handler = handler;
+
+//    // Cause the signal to become masked when its handler is running
+//    sigemptyset(&act.sa_mask);
+//    sigaddset(&act.sa_mask, sig);
+
+//    sigaction(sig, &act, &old_act);
+
+    errno = ENOSYS;
+
+    return nullptr;
+}
